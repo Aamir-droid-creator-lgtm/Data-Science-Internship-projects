dcbbcce53b382bf7bea5d317ac7457a5fef0c8d2
Aamir Pathan <royal15914@gmail.com>
2025-12-17 20:06:30 +0530

    Add personal introduction program in Python

---
+++ week01_python_basics/personal_intro.py
@@ -0,0 +1,16 @@
+# Project: Personal Introduction Program
+
+print("Welcome to the personal introduction program!")
+
+name = input("What is your name? ")
+age = input("What is your your age? ")
+city = input("Which city are you from? ")
+hobby = input("What is your favourite hobby? ")
+college = input("Which college are you studying in? ")
+goals = input("What is your career goal? ")
+
+print("\n🎉 Welcome", name + "!")
+print(f"You are {age} years old and you live in {city}")
+print(f"It's great to know that you enjoy {hobby} and study at {college}")
+print(f"It seems to be great that your career goal is to become a {goals}")
+print(f"Thanks {name} for sharing your personal details. Have a great day! ")
